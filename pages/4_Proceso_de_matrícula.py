2229290bdb47da4ce5c59c11b79c4916310525a7
terrilevi <118628583+terrilevi@users.noreply.github.com>
2024-04-28 10:51:17 -0500

    Update 4_Proceso_de_matrícula.py

--- pages/4_Proceso_de_matrícula.py
+++ pages/4_Proceso_de_matrícula.py
@@ -19,22 +19,25 @@ def draw_graph():
 
         G = nx.DiGraph()
         for index, row in df_filtrado.iterrows():
-            # Ensure node is added only once and color is updated correctly
             if row['Código'] not in G.nodes():
                 G.add_node(row['Código'], title=row['Código'], color='green' if row['Código'] in cursos_aprobados else 'gray')
             if row['Requisito'] != 'Ninguno':
                 if row['Requisito'] not in G.nodes():
                     G.add_node(row['Requisito'], title=row['Requisito'], color='green' if row['Requisito'] in cursos_aprobados else 'gray')
                 G.add_edge(row['Requisito'], row['Código'])
-                # Recheck node color for courses with prerequisites
-                if row['Requisito'] in cursos_aprobados:
-                    G.nodes[row['Código']]['color'] = 'blue' if row['Código'] not in cursos_aprobados else 'green'
+                if row['Requisito'] in cursos_aprobados and row['Código'] not in cursos_aprobados:
+                    G.nodes[row['Código']]['color'] = 'blue'
+
+        nodos_mostrados = G.nodes()
+        df_mostrados = df[df['Código'].isin(nodos_mostrados)].copy()
+        df_mostrados = df_mostrados[['Ciclo', 'Código', 'Nombre']].drop_duplicates().sort_values(by='Ciclo')
+        
+        st.write("Courses Displayed in the Graph")
+        st.dataframe(df_mostrados)
 
         net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white")
         for node, node_attrs in G.nodes(data=True):
             net.add_node(node, title=node, color=node_attrs['color'])
-            print(f"Node: {node}, Color: {node_attrs['color']}")  # Debug output
-
         for edge in G.edges():
             net.add_edge(edge[0], edge[1])
 
@@ -43,7 +46,7 @@ def draw_graph():
         source_code = HtmlFile.read()
         st.components.v1.html(source_code, height=800)
     else:
-        st.error("Revisaaaa")
+        st.error("Please ensure all necessary data is loaded.")
 
 
 def main():
@@ -57,7 +60,7 @@ def main():
                 st.write(f"Cursos: {st.session_state['cursos_aprobados']}")
                 st.write(f"Ciclo actual: {st.session_state['ciclo_actual']}")
                 df = st.session_state['df']
-                draw_graph()  # Now we draw the graph after the checks
+                draw_graph() 
     else:
         st.error("Debes iniciar sesión para ver el contenido.")
 
