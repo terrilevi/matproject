333f382b43a24c714ed0f470d296c7422e36e0bd
terrilevi <118628583+terrilevi@users.noreply.github.com>
2024-04-27 20:50:00 -0500

    Update login.py

--- login.py
+++ login.py
@@ -6,22 +6,31 @@ import os
 #Base de datos de alumno
 file_path = os.path.join(os.path.dirname(__file__), 'bd_alumnos.csv')
 df_alumnos = pd.read_csv(file_path)
+
 ########################
 
 
 
 ##############Sistema de Login##########################
 def entrada_credencial():
-    # Get the credentials provided by the user
     user_input = st.session_state.get("user", "").strip()
     passwd_input = st.session_state.get("passwd", "").strip()
     
     # Validate credentials
     if any((df_alumnos['userid'] == user_input) & (df_alumnos['password'] == passwd_input)):
         st.session_state["autenticacion"] = True
+
+        ####Extraccion de columnas por usuario para luego trabajar con ellas#####
+        fila_de_usuarios = df_alumnos[df_alumnos['userid'] == user_input].iloc[0]
+        st.session_state['nombre'] = fila_de_usuarios['nombre']
+        st.session_state['ciclo_actual'] = fila_de_usuarios['ciclo_actual']
+        st.session_state['cursos_aprobados'] = fila_de_usuarios['cursos_aprobados']
+
+        ##########################################################################
+        
     else:
         st.session_state["autenticacion"] = False
-        st.error("Contraseña/Usuario invalido")  # Show error if credentials are incorrect
+        st.error("Contraseña/Usuario inválido")
 
 
 
@@ -44,3 +53,4 @@ def autenticacion_usuario():
         entrada_credencial()  # Check credentials directly when the button is clicked
 
     return st.session_state.get("autenticacion", False)
+
