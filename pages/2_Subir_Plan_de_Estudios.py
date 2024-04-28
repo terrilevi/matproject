193e7d619121c7f1163590d9013d44874656bbf8
terrilevi <118628583+terrilevi@users.noreply.github.com>
2024-04-28 08:38:53 -0500

    Update 2_Subir_Plan_de_Estudios.py

--- pages/2_Subir_Plan_de_Estudios.py
+++ pages/2_Subir_Plan_de_Estudios.py
@@ -3,20 +3,47 @@ import pandas as pd
 
 from login import autenticacion_usuario
 
+# Función que permite verificar que los nombres de las columnas, como parámetro, estén en el DataFrame
+def verificarDataFrame(df):
+    nombresColumnas = ['Ciclo', 'Código', 'Nombre', 'Requisito', 'Nombre Requisito', 'Tipo', 'Sede', 'Modalidad', 'Créditos']
+    for columna in nombresColumnas:
+        if columna not in df.columns:
+            raise ValueError(f"El DataFrame no tiene la columna requerida: {columna}")
+    return df
+
+
+# Función que permite la lectura de DataFrame
+def leerDataFrame(upload_file):
+    filename = upload_file.name  
+    extension = filename.split('.')[-1]  
+    if extension == 'csv':
+        data = pd.read_csv(upload_file)
+    elif extension == 'xlsx':
+        data = pd.read_excel(upload_file)
+    else:
+        raise ValueError("¡Este tipo de archivo no es admitido!")
+
+    verificarDataFrame(data)
+
+    return data
+
 
 def main():
     if autenticacion_usuario():
         st.title("Plan de estudios")
         upload_file = st.file_uploader('Subir el plan de estudios correspondiente a tu carrera')
         if upload_file is not None:
-            df = pd.read_csv(upload_file)
-            st.session_state['df']= df
+            filename = upload_file.name  # Obtener el nombre del archivo
+            st.write(f"Archivo cargado: {filename}")  # Mostrar el nombre del archivo
+            try:
+                df = leerDataFrame(upload_file)
+                st.session_state['df'] = df
+                st.write("¡Archivo cargado y verificado correctamente!")
+                st.write("(●'◡'●)")
+            except ValueError as error:
+                st.error(f"Error al cargar el archivo: {error}")
     else:
         st.error("Debes iniciar sesión para ver el contenido.")
-
-
-    
-
 
 
 if __name__ == "__main__":
